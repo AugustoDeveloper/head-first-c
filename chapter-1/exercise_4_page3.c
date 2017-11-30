003a7f1747d12bb6d7133d2eb13e8c487d509b75
Augusto Cesar <abatista@tasken.com>
2017-11-29 23:47:33 -0200

    added new exercise 4 from page 3

---
+++ chapter-1/exercise_4_page3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  char suit = 'H';
+  switch(suit) {
+    case 'C': puts("Clubs");
+    break;
+    case 'D': puts("Diamonds");
+    break;
+    case 'H': puts("Hearts");
+    break;
+    default:
+    puts("Spades");
+  }
+  return 0;
+}
